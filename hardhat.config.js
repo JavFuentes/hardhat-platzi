fc2b905a0fd8e763cd3c63c0ff1d569ecbc23d3b
JavFuentes <javier.antonio8128@gmail.com>
2024-06-23 13:53:39 -0400

    add secrets.json to .gitignore

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,18 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+const { infuraProjectId, privateKey } = require("./secrets.json");
+
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://linea-sepolia.infura.io/v3/${infuraProjectId}`,
+      accounts: [privateKey]
+    }
+  },
+  etherscan: {
+    apiKey: "YOUR_ETHERSCAN_API_KEY"  // Si tienes una API Key de Etherscan, agrégala aquí
+  }
 };
